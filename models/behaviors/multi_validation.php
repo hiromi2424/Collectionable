90f8fee40c29dc15248a0798a6f9e545a09052d7
hiromi2424 <tanaka2424@gmail.com>
2011-02-10 05:36:44 +0900

    MultiValidation Behavior is now available.

---
+++ models/behaviors/multi_validation.php
@@ -0,0 +1,100 @@
+<?php
+
+class MultiValidationBehavior extends ModelBehavior {
+
+	public $settings = array();
+	public static $defaultSettings = array(
+		'restore' => true,
+	);
+
+	public $mapMethods = array(
+		'/^use(.+?)Validation$/' => 'useValidationSet',
+	);
+
+	protected $_backupValidate = array();
+
+	public function setup($Model, $settings = array()) {
+
+		$this->settings[$Model->alias] = array_merge(self::$defaultSettings, (array)$settings);
+		return true;
+
+	}
+
+	public function restoreValidate($Model) {
+
+		if (isset($this->_backupValidate[$Model->alias])) {
+			$Model->validate = $this->_backupValidate[$Model->alias];
+			unset($this->_backupValidate[$Model->alias]);
+		}
+
+	}
+
+	public function afterSave($Model, $created = true, $options = array()) {
+
+		if ($this->settings[$Model->alias]['restore']) {
+			$this->restoreValidate($Model);
+		}
+
+		return true;
+
+	}
+
+	public function useValidationSet($Model, $method, $useBase = true) {
+
+		if (is_array($method) || !preg_match(key($this->mapMethods), $method, $matches)) {
+			$validates = array_map('ucfirst', (array)$method);
+		} else {
+			$validates = explode('And', $matches[1]);
+		}
+
+		$result = array();
+		foreach ($validates as $validate) {
+
+			$property = 'validate' . $validate;
+			if (!isset($Model->{$property}) && !property_exists($Model, $property)) {
+				throw new MultiValidation_PropertyNotFoundException($property);
+			}
+			$result = $this->mergeValidationSet($Model, $result, $Model->{$property});
+
+		}
+
+		if ($useBase) {
+			$result = $this->mergeValidationSet($Model, $Model->validate, $result);
+		}
+
+		$this->_backupValidate[$Model->alias] = $Model->validate;
+		$Model->validate = $result;
+
+	}
+
+	public function mergeValidationSet($Model) {
+
+		$validationSets = func_get_args();
+		/* $Model = */ array_shift($validationSets);
+
+		$result = array();
+		foreach ($validationSets as $validationSet) {
+			foreach ($validationSet as $field => $ruleSet) {
+				foreach ($ruleSet as $name => $rules) {
+					if (isset($result[$field][$name])) {
+						$result[$field][$name] = array_merge($result[$field][$name], $rules);
+					} else {
+						$result[$field][$name] = $rules;
+					}
+				}
+			}
+		}
+
+		return $result;
+
+	}
+
+}
+
+class MultiValidation_PropertyNotFoundException extends Exception {
+
+	public function __construct($message = null, $code = 0, $previous = null) {
+		parent::__construct(__d('collectionable', 'Unexpected property name: Model::$%s was not found.', $message), $code, $previous);
+	}
+
+}
